b1419b9e2e2d207f6dc3a6c3a921369aca4730b5
Felipe Trindade <ftrindade-external@altoslabs.com>
2023-11-07 10:55:02 -0300

    fix data modeling

--- src/config/database/migrations/20231103152523_create_base_tables.sql
+++ src/config/database/migrations/20231103152523_create_base_tables.sql
@@ -1,6 +1,6 @@
 -- migrate:up
 CREATE TABLE poll (
-    poll_id varchar(36), -- UUID
+    poll_id VARCHAR(36), -- UUID
     question VARCHAR(500) NOT NULL,
     multi_choice BOOLEAN NOT NULL,
     creation_date DATETIME DEFAULT CURRENT_TIMESTAMP,
@@ -9,7 +9,7 @@ CREATE TABLE poll (
 
 CREATE TABLE choice (
     choice_id INT AUTO_INCREMENT,
-    poll_id varchar(36),
+    poll_id VARCHAR(36),
     choice VARCHAR(255) NOT NULL,
     PRIMARY KEY (choice_id),
     FOREIGN KEY (poll_id) REFERENCES poll(poll_id)
@@ -17,12 +17,21 @@ CREATE TABLE choice (
 
 CREATE TABLE vote (
     vote_id INT AUTO_INCREMENT,
+    vote_date DATETIME DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY (vote_id)
+);
+
+CREATE TABLE choice_vote (
+    choice_vote_id INT AUTO_INCREMENT,
     choice_id INT,
-    PRIMARY KEY (vote_id),
-    FOREIGN KEY (choice_id) REFERENCES choice(choice_id)
-)
+    vote_id INT,
+    PRIMARY KEY (choice_vote_id),
+    FOREIGN KEY (choice_id) REFERENCES choice(choice_id),
+    FOREIGN KEY (vote_id) REFERENCES vote(vote_id)
+);
 
 -- migrate:down
+DROP TABLE choice_vote;
 DROP TABLE vote;
 DROP TABLE choice;
 DROP TABLE poll;
