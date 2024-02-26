c714f6c8f0f8a1829a62162cfe5850d0c5e8d72b
Felipe Trindade <felipe@loka.com>
2024-02-26 16:43:41 -0300

    wip

--- src/config/environments.ts
+++ src/config/environments.ts
@@ -16,5 +16,3 @@ export const MAX_NUMBER_OF_CHOICES = process.env.MAX_NUMBER_OF_CHOICES === undef
 
 // OBSERVABILITY
 export const LOGGER_LEVEL = process.env.LOGGER_LEVEL ?? 'info'
-export const OTEL_EXPORTER_OTLP_ENDPOINT = process.env.OTEL_EXPORTER_OTLP_ENDPOINT
-export const METRICS_PORT = process.env.METRICS_PORT ?? '9464'
